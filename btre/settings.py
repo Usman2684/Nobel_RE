63d5bbb4b54aea629aafb02fa96e86d760230d71
Usman <usmanw2684@gmail.com>
2021-01-18 09:19:46 +0500

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -150,3 +150,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'usmanw2684@gmail.com'
 EMAIL_HOST_PASSWORD = 'UsMan5865;'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
